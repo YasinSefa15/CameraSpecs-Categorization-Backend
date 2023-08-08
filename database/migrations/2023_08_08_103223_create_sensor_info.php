0a92253e1d1083d0b29dc3943efed81a6b92055f
YasinSefa15 <yasekirman@gmail.com>
2023-08-08 16:17:59 +0300

    Migrations/Models created, relations added, seeder wrotten

---
+++ database/migrations/2023_08_08_103223_create_sensor_info.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sensor_info', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('camera_id')->constrained('cameras');
+            $table->string('sensor', 32);
+            $table->unsignedDouble('diagonal');
+            $table->string('surface_area', 32);
+            $table->string('pixel_pitch', 32);
+            $table->string('pixel_area', 32);
+            $table->string('pixel_density', 32);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sensor_info');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Brand;
+use App\Models\Camera;
+use App\Models\Specification;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,38 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Brand::factory(5)
+            ->create()
+            ->each(function ($brand) {
+                Camera::factory(fake()->numberBetween(50, 300))
+                    ->create([
+                        'brand_id' => $brand->id,
+                    ])
+                    ->each(function ($camera) {
+                        $camera->images()->save(\App\Models\Image::factory()->make([
+                            'taggable_type' => 'App\Models\Camera',
+                        ]));
+
+                        $camera->sensor_info()->save(\App\Models\SensorInfo::factory()->make([
+                            'camera_id' => $camera->id,
+                        ]));
+
+                        $specifications = [];
+
+                        $specifications[] = [
+                            'title' => fake()->title,
+                            'value' => 'x',
+                            'camera_id' => $camera->id,
+                        ];
+
+                        //$camera->specifications()->save(\App\Models\Specification::query()->insert($specifications));
+                        Specification::query()->insert($specifications);
+                    });
+
+                $brand->images()->save(\App\Models\Image::factory()->make([
+                    'taggable_type' => 'App\Models\Brand',
+                ]));
+            });
     }
 }
